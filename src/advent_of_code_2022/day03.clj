014e32d5e8f86e9115d7cd278d8a8fa326547756
tvirolai <tuomo.virolainen@siili.com>
2022-12-03 09:54:51 +0200

    Clean up the solution a bit

--- src/advent_of_code_2022/day03.clj
+++ src/advent_of_code_2022/day03.clj
@@ -3,13 +3,7 @@
             [clojure.set :as cset]))
 
 (def input
-  "resources/day03.txt")
-
-(def test-input
-  "resources/day03_test.txt")
-
-(defn parse-input [file]
-  (s/split (slurp file) #"\n"))
+  (s/split (slurp "resources/day03.txt") #"\n"))
 
 (def priority-map
   (let [downcase (zipmap (range (int \a) (inc (int \z))) (range 1 27))
@@ -26,11 +20,10 @@
     (first (cset/intersection (set hd) (set tl)))))
 
 (defn part-01 []
-  (let [data (parse-input input)]
-    (->> data
-         (map get-shared-item)
-         (map get-priority)
-         (reduce +))))
+  (->> input
+       (map get-shared-item)
+       (map get-priority)
+       (reduce +)))
 
 (defn get-shared-item-02 [lines]
   (->> lines
@@ -39,8 +32,8 @@
        first))
 
 (defn part-02 []
-  (let [data (partition 3 (parse-input input))]
-    (->> data
-         (map get-shared-item-02)
-         (map get-priority)
-         (reduce +))))
+  (->> input
+       (partition 3)
+       (map get-shared-item-02)
+       (map get-priority)
+       (reduce +)))
